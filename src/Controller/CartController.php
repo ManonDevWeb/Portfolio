2dbf95c695f1d4d3fff469427e584b112a250e71
Cloclo <cloclodevo@gmail.com>
2022-05-23 14:57:52 +0200

    ajout du bouton valider le panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,17 +3,42 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+
+    private $entityManager; 
+
+    //nécessaire pour appeler le getRepository dans index et avoir accès aux données de produits
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/mon-panier', name: 'app_cart')]
     public function index(Cart $cart): Response
     {
+        //Variable stockant toutes les infos liées au panier (id, nom, ...)
+        $cartComplete = [];
+        //Pour chaque élément dans le panier
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        //dd($cartComplete);
+
         //dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
 
