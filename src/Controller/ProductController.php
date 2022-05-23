f3089d810311ea5e535e60430449fb5d9d009027
Cloclo <cloclodevo@gmail.com>
2022-05-23 11:21:38 +0200

    Ajout d'un filtre produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,11 +22,17 @@ class ProductController extends AbstractController
     #[Route('/nos-produits', name: 'app_product')]
     public function index(): Response
     {
+
+        //FILTRE
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
         //Récupérer toutes les données grâce au repository
         $products = $this->entityManager->getRepository(Product::class)->findAll();
         // dd($products);
         return $this->render('product/index.html.twig', [
-            'products'=>$products
+            'products' => $products,
+            'form' => $form->createView()
         ]
     );
     }
